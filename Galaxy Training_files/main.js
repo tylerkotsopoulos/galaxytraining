2ea637fd34af233c383bdf422416d0bdd232ab07
Kevin Ng <kevin@kevinng.ca>
2020-03-24 14:25:20 -0400

    initial commit

---
+++ Galaxy Training_files/main.js
@@ -0,0 +1,199 @@
+var galaxy = galaxy || {};
+$(function() {
+    galaxy.init();
+});
+
+galaxy.init = function(){
+	// galaxy.fullpage();
+	galaxy.navigation();
+	// galaxy.midnight();
+	galaxy.smoothScroll();
+	galaxy.parallax();
+	// galaxy.par();
+	galaxy.lightbox();
+	//galaxy.initMap();
+	galaxy.header();
+	galaxy.mobileMenu();
+}
+
+galaxy.midnight = function(){
+	$('.fixednav').midnight();
+}
+
+galaxy.navigation = function(){
+	// $('.dot').click(function(){
+	//    $(".dot.current").removeClass("current"); 
+	//    $(this).addClass("current"); 
+	// });
+	var contentSections = $('.cd-section'),
+		navigationItems = $('#cd-vertical-nav a');
+
+	updateNavigation();
+	$(window).on('scroll', function(){
+		updateNavigation();
+	});
+
+	//smooth scroll to the section
+	navigationItems.on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+    //smooth scroll to second section
+    $('.cd-scroll-down').on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    //open-close navigation on touch devices
+    $('.touch .cd-nav-trigger').on('click', function(){
+    	$('.touch #cd-vertical-nav').toggleClass('open');
+  
+    });
+    //close navigation on touch devices when selectin an elemnt from the list
+    $('.touch #cd-vertical-nav a').on('click', function(){
+    	$('.touch #cd-vertical-nav').removeClass('open');
+    });
+
+	function updateNavigation() {
+		contentSections.each(function(){
+			$this = $(this);
+			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+				navigationItems.eq(activeSection).addClass('is-selected');
+			}else {
+				navigationItems.eq(activeSection).removeClass('is-selected');
+			}
+		});
+	}
+
+	function smoothScroll(target) {
+        $('body,html').animate(
+        	{'scrollTop':target.offset().top},
+        	600
+        );
+	}
+}
+
+galaxy.par = function(){
+	 window.onscroll = function(){   
+     document.body.style.msPerspectiveOrigin =
+     document.body.style.mozPerspectiveOrigin =
+     document.body.style.webkitPerspectiveOrigin =
+     window.scrollX + "px " + 
+     window.scrollY + "px";
+   }
+}
+
+galaxy.mobileMenu = function(){
+	var $hamburger = $(".hamburger");
+	 $hamburger.on("click", function(e) {
+	    $hamburger.toggleClass("is-active");
+	    e.preventDefault();
+	    if ($('.header-mobile').toggleClass('active')) {
+	    }
+	 });
+
+	var link = $('.header-mobile .navigation-link'),
+		social = $('.header-mobile .hero-social__icon');
+	$(link).click(function(e){
+		// e.preventDefault();
+		$('.header-mobile').removeClass('active');
+		$hamburger.removeClass('is-active');
+		// alert('works');
+	});
+	$(social).click(function(e){
+		$('.header-mobile').removeClass('active');
+	});
+}
+
+galaxy.parallax = function(){
+	var parallax = document.querySelectorAll('.parallax-section'),
+		speed = 0.5;
+	window.onscroll = function(){
+		[].slice.call(parallax).forEach(function(el,i){
+			var windowYOffset = window.pageYOffset,
+				elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+			el.style.backgroundPosition = elBackgrounPos;
+		});
+	};
+}
+
+galaxy.smoothScroll = function(){
+	smoothScroll.init({
+	    speed: 500,
+	    easing: 'easeInOutCubic',
+	    offset: 60
+	});
+}
+
+galaxy.lightbox = function(){
+	lightbox.option({
+	   'resizeDuration': 600,
+	   'wrapAround': true
+	})
+}
+
+galaxy.fullpage = function(){
+	$('#fullpage').fullpage({
+		anchors: ['1', '2', '3', '4', '5'],
+		// lockAnchors: false,
+		autoScrolling: false,
+        fitToSection: false,
+		navigation: true,
+		showActiveTooltip: false,
+	    slidesNavigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['Home', 'What We Do', 'Head Trainer', 'Gallery', 'Contact Us'],
+		css3: true,
+		fixedElements: '.header'
+	});
+}
+
+galaxy.header = function(){
+	var flag = $('#header-fade').offset().top;
+	$(document).scroll(function(){
+		if($(window).height() + $(this).scrollTop() > flag){
+			$('.header').addClass('bg--shaded-transparent');
+		}else{
+			$('.header').removeClass('bg--shaded-transparent');
+		}
+	});
+}
+
+galaxy.map = function(){
+	var location = {lat: 43.83115, lng: -79.0834645};
+	var map = new google.maps.Map(document.getElementById('map'), {
+	    zoom: 15,
+	    center: location
+	});
+	var marker = new google.maps.Marker({
+	    position: location,
+	    map: map
+	});
+	marker.addListener('click', function() {
+	    console.log('clicked!');
+	    var url = 'https://web.archive.org/web/20180827152549/https://goo.gl/maps/pj92wMxraBE2';
+	    window.open(url, '_blank');
+	});
+}
+/*
+     FILE ARCHIVED ON 15:25:49 Aug 27, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 16:06:51 Mar 24, 2020.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  esindex: 0.012
+  PetaboxLoader3.resolve: 242.126 (3)
+  exclusion.robots.policy: 0.161
+  captures_list: 258.339
+  RedisCDXSource: 0.569
+  LoadShardBlock: 240.078 (3)
+  exclusion.robots: 0.172
+  load_resource: 535.966
+  PetaboxLoader3.datanode: 530.124 (5)
+  CDXLines.iter: 14.344 (3)
+*/
---
+++ Galaxy Training_files/util.js
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Wayback Machine</title>
+<script src="//archive.org/includes/analytics.js?v=cf34f82" type="text/javascript"></script>
+<script type="text/javascript">window.addEventListener('DOMContentLoaded',function(){var v=archive_analytics.values;v.service='wb';v.server_name='wwwb-app101.us.archive.org';v.server_ms=476;archive_analytics.send_pageview({});});</script><link rel="stylesheet" type="text/css" href="/_static/css/banner-styles.css" />
+<link rel="stylesheet" type="text/css" href="/_static/css/iconochive.css" />
+<script src="/_static/js/jquery-1.11.1.min.js"></script>
+</head>
+<body style="overflow:hidden;">
+<!-- BEGIN WAYBACK TOOLBAR INSERT -->
+<script type="text/javascript" src="/_static/js/timestamp.js" charset="utf-8"></script>
+<script type="text/javascript" src="/_static/js/graph-calc.js" charset="utf-8"></script>
+<script type="text/javascript" src="/_static/js/auto-complete.js" charset="utf-8"></script>
+<script type="text/javascript" src="/_static/js/toolbar.js" charset="utf-8"></script>
+<style type="text/css">
+body {
+  margin-top:0 !important;
+  padding-top:0 !important;
+  /*min-width:800px !important;*/
+}
+.wb-autocomplete-suggestions {
+    text-align: left; cursor: default; border: 1px solid #ccc; border-top: 0; background: #fff; box-shadow: -1px 1px 3px rgba(0,0,0,.1);
+    position: absolute; display: none; z-index: 2147483647; max-height: 254px; overflow: hidden; overflow-y: auto; box-sizing: border-box;
+}
+.wb-autocomplete-suggestion { position: relative; padding: 0 .6em; line-height: 23px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; font-size: 1.02em; color: #333; }
+.wb-autocomplete-suggestion b { font-weight: bold; }
+.wb-autocomplete-suggestion.selected { background: #f0f0f0; }
+</style>
+<div id="wm-ipp-base" lang="en" style="display:none;direction:ltr;">
+<div id="wm-ipp" style="position:fixed;left:0;top:0;right:0;">
+<div id="wm-ipp-inside">
+  <div style="position:relative;">
+    <div id="wm-logo" style="float:left;width:130px;padding-top:10px;">
+      <a href="/web/" title="Wayback Machine home page"><img src="/_static/images/toolbar/wayback-toolbar-logo.png" alt="Wayback Machine" width="110" height="39" border="0" /></a>
+    </div>
+    <div class="r" style="float:right;">
+      <div id="wm-btns" style="text-align:right;height:25px;">
+                  <div id="wm-save-snapshot-success">success</div>
+          <div id="wm-save-snapshot-fail">fail</div>
+          <a href="#"
+             onclick="__wm.saveSnapshot('https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js', '20180423205201')"
+             title="Share via My Web Archive"
+             id="wm-save-snapshot-open"
+          >
+            <span class="iconochive-web"></span>
+          </a>
+          <a href="https://archive.org/account/login.php"
+             title="Sign In"
+             id="wm-sign-in"
+          >
+            <span class="iconochive-person"></span>
+          </a>
+          <span id="wm-save-snapshot-in-progress" class="iconochive-web"></span>
+        	<a href="http://faq.web.archive.org/" title="Get some help using the Wayback Machine" style="top:-6px;"><span class="iconochive-question" style="color:rgb(87,186,244);font-size:160%;"></span></a>
+	<a id="wm-tb-close" href="#close" onclick="__wm.h(event);return false;" style="top:-2px;" title="Close the toolbar"><span class="iconochive-remove-circle" style="color:#888888;font-size:240%;"></span></a>
+      </div>
+      <div id="wm-share">
+          <a href="/web/20180423205201/http://web.archive.org/screenshot/https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js"
+             id="wm-screenshot"
+             title="screenshot">
+              <span class="wm-icon-screen-shot"></span>
+          </a>
+	<a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=https://web.archive.org/web/20180423205201/https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js', '', 'height=400,width=600'); return false;" title="Share on Facebook" style="margin-right:5px;" target="_blank"><span class="iconochive-facebook" style="color:#3b5998;font-size:160%;"></span></a>
+	<a href="#" onclick="window.open('https://twitter.com/intent/tweet?text=https://web.archive.org/web/20180423205201/https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js&amp;via=internetarchive', '', 'height=400,width=600'); return false;" title="Share on Twitter" style="margin-right:5px;" target="_blank"><span class="iconochive-twitter" style="color:#1dcaff;font-size:160%;"></span></a>
+      </div>
+    </div>
+    <table class="c" style="">
+      <tbody>
+	<tr>
+	  <td class="u" colspan="2">
+	    <form target="_top" method="get" action="/web/submit" name="wmtb" id="wmtb"><input type="text" name="url" id="wmtbURL" value="https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js" onfocus="this.focus();this.select();" /><input type="hidden" name="type" value="replay" /><input type="hidden" name="date" value="20180423205201" /><input type="submit" value="Go" /></form>
+	  </td>
+	  <td class="n" rowspan="2" style="width:110px;">
+	    <table>
+	      <tbody>
+		<!-- NEXT/PREV MONTH NAV AND MONTH INDICATOR -->
+		<tr class="m">
+		  <td class="b" nowrap="nowrap">Mar</td>
+		  <td class="c" id="displayMonthEl" title="You are here: 20:52:01 Apr 23, 2018">APR</td>
+		  <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20180710171345/https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js" title="10 Jul 2018"><strong>Jul</strong></a></td>
+		</tr>
+		<!-- NEXT/PREV CAPTURE NAV AND DAY OF MONTH INDICATOR -->
+		<tr class="d">
+		  <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/20180422205135/https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js" title="20:51:35 Apr 22, 2018"><img src="/_static/images/toolbar/wm_tb_prv_on.png" alt="Previous capture" width="14" height="16" border="0" /></a></td>
+		  <td class="c" id="displayDayEl" style="width:34px;font-size:24px;white-space:nowrap;" title="You are here: 20:52:01 Apr 23, 2018">23</td>
+		  <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20180710171345/https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js" title="17:13:45 Jul 10, 2018"><img src="/_static/images/toolbar/wm_tb_nxt_on.png" alt="Next capture" width="14" height="16" border="0" /></a></td>
+		</tr>
+		<!-- NEXT/PREV YEAR NAV AND YEAR INDICATOR -->
+		<tr class="y">
+		  <td class="b" nowrap="nowrap">2017</td>
+		  <td class="c" id="displayYearEl" title="You are here: 20:52:01 Apr 23, 2018">2018</td>
+		  <td class="f" nowrap="nowrap">2019</td>
+		</tr>
+	      </tbody>
+	    </table>
+	  </td>
+	</tr>
+	<tr>
+	  <td class="s">
+	    	    <div id="wm-nav-captures">
+	      	      <a class="t" href="/web/20180423205201*/https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js" title="See a list of every capture for this URL">2,911 captures</a>
+	      <div class="r" title="Timespan for captures of this URL">18 Apr 2018 - 12 Feb 2019</div>
+	      </div>
+	  </td>
+	  <td class="k">
+	    <a href="" id="wm-graph-anchor">
+	      <div id="wm-ipp-sparkline" title="Explore captures for this URL" style="position: relative">
+		<canvas id="wm-sparkline-canvas" width="625" height="27" border="0"></canvas>
+	      </div>
+	    </a>
+	  </td>
+	</tr>
+      </tbody>
+    </table>
+    <div style="position:absolute;bottom:0;right:2px;text-align:right;">
+      <a id="wm-expand" class="wm-btn wm-closed" href="#expand" onclick="__wm.ex(event);return false;"><span id="wm-expand-icon" class="iconochive-down-solid"></span> <span style="font-size:80%">About this capture</span></a>
+    </div>
+  </div>
+    <div id="wm-capinfo" style="border-top:1px solid #777;display:none; overflow: hidden">
+            <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center">COLLECTED BY</div>
+    <div style="padding:3px;position:relative" id="wm-collected-by-content">
+            <div style="display:inline-block;vertical-align:top;width:50%;">
+			<span class="c-logo" style="background-image:url(https://archive.org/services/img/webwidecrawl);"></span>
+		Organization: <a style="color:#33f;" href="https://archive.org/details/webwidecrawl" target="_new"><span class="wm-title">Internet Archive</span></a>
+		<div style="max-height:75px;overflow:hidden;position:relative;">
+	  <div style="position:absolute;top:0;left:0;width:100%;height:75px;background:linear-gradient(to bottom,rgba(255,255,255,0) 0%,rgba(255,255,255,0) 90%,rgba(255,255,255,255) 100%);"></div>
+	  The Internet Archive discovers and captures web pages through many different web crawls.
+
+At any given time several distinct crawls are running, some for months, and some every day or longer.
+
+View the web archive through the <a href="http://archive.org/web/web.php">Wayback Machine</a>.
+	</div>
+	      </div>
+      <div style="display:inline-block;vertical-align:top;width:49%;">
+			<span class="c-logo" style="background-image:url(https://archive.org/services/img/liveweb)"></span>
+		<div>Collection: <a style="color:#33f;" href="https://archive.org/details/liveweb" target="_new"><span class="wm-title">Live Web Proxy Crawls</span></a></div>
+		<div style="max-height:75px;overflow:hidden;position:relative;">
+	  <div style="position:absolute;top:0;left:0;width:100%;height:75px;background:linear-gradient(to bottom,rgba(255,255,255,0) 0%,rgba(255,255,255,0) 90%,rgba(255,255,255,255) 100%);"></div>
+	  Content crawled via the <a href="http://archive.org/web/web.php">Wayback Machine</a> Live Proxy mostly by the Save Page Now feature on web.archive.org.
+<br /><br />
+Liveweb proxy is a component of Internet Archive’s wayback machine project. The liveweb proxy captures the content of a web page in real time, archives it into a ARC or WARC file and returns the ARC/WARC record back to the wayback machine to process. The recorded ARC/WARC file becomes part of the wayback machine in due course of time.
+<br /><br />
+	</div>
+	      </div>
+    </div>
+    <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center" title="Timestamps for the elements of this page">TIMESTAMPS</div>
+    <div>
+      <div id="wm-capresources" style="margin:0 5px 5px 5px;max-height:250px;overflow-y:scroll !important"></div>
+      <div id="wm-capresources-loading" style="text-align:left;margin:0 20px 5px 5px;display:none"><img src="/_static/images/loading.gif" alt="loading" /></div>
+    </div>
+  </div></div></div></div><div id="donato" style="position:relative;width:100%;">
+  <div id="donato-base">
+    <iframe id="donato-if" src="https://archive.org/includes/donate.php?as_page=1&amp;transpiled=0&amp;referer=https%3A//web.archive.org/web/20180423205201/https%3A//maps.googleapis.com/maps-api-v3/api/js/32/11/util.js"
+	    scrolling="no" frameborder="0" style="width:100%; height:100%">
+    </iframe>
+  </div>
+</div><script type="text/javascript">
+__wm.bt(625,27,25,2,"web","https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js","2018-04-23",1996,"/_static/",['css/banner-styles.css','css/iconochive.css']);
+__wm.checkScreenShot("https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js", '20180423205201');
+</script>
+<!-- END WAYBACK TOOLBAR INSERT --><iframe id="playback" src="https://web.archive.org/web/20180423205201if_/https://maps.googleapis.com/maps-api-v3/api/js/32/11/util.js" frameborder="0" style="position:absolute;top:65px;left:0;width:100%;">
+</iframe>
+<script type="text/javascript">
+var margin_top = 65;
+function fitPlayback() {
+  $('#playback').css({
+    height: (window.innerHeight - margin_top) + 'px',
+    top: margin_top
+  });
+}
+fitPlayback();
+$(window).on('resize', function() {
+  fitPlayback(); 
+});
+$('#wm-tb-close').click(function(){
+  margin_top = 0;
+  fitPlayback();
+});
+</script>
+</body>
+</html>
